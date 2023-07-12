6cf36e23aba898b295660bb7e5bc75546894c7e8
Sritycseru <94358539+Sritycseru@users.noreply.github.com>
2023-07-12 18:51:21 +0600

    Add files via upload

---
+++ PSK.m
@@ -0,0 +1,48 @@
+clc; 
+clear all;
+close all; 
+bits = [1 0 1 1 0 0 1];
+
+% Modulation
+bitrate = 1;
+
+sampling_rate = 100;
+sampling_time = 1/sampling_rate;
+
+end_time = length(bits)/bitrate;
+time = 0:sampling_time:end_time;
+
+a = 3;  %Amplitude
+f = 2;   %Frequency
+signal = a*sin(2*pi*f*time);
+modulation = signal;
+
+index = 1;
+
+for i = 1:length(time)
+    if bits(index) == 0
+        modulation(i) = -signal(i);
+    end
+    if time(i)*bitrate >= index
+        index = index+1;
+    end
+end
+
+plot(time, modulation);
+axis([0 end_time -2*a 2*a]);
+grid on;
+
+%Demodulation
+index = 1;
+
+for i = 1:length(modulation)
+    demodultaion(index) = 1;
+    if modulation(i) ~= signal(i)
+        demodultaion(index) = 0;
+    end
+    if time(i)*bitrate >= index
+        index = index+1;
+    end
+end
+
+disp(demodultaion);
